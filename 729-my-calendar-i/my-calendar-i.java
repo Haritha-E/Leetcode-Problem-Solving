2b004e1f0491aaed3f7bbee70ad577f33a2910e4
Haritha Eswaran <125653563+Haritha-E@users.noreply.github.com>
2024-09-26 22:10:21 +0530

    Time: 19 ms (81.41%) | Memory: 45.5 MB (42.73%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.java
@@ -0,0 +1,27 @@
+class MyCalendar {
+        TreeSet<int[]> st;
+
+    public MyCalendar() {
+         st = new TreeSet<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+    }
+    
+    public boolean book(int start, int end) {
+      int[] event = new int[]{start, end};
+        int[] next = st.ceiling(event); // O(logN)
+        if (next != null && next[0] < end) {
+            return false;
+        }
+        int[] prev = st.floor(event);
+        if (prev != null && start < prev[1]) {
+            return false;
+        }
+        st.add(new int[]{start, end});
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
