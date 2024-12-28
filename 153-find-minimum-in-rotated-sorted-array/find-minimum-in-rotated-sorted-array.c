0ccd2825701fb2a7bb1f384757cba4fa1a392874
Haritha Eswaran <125653563+Haritha-E@users.noreply.github.com>
2024-12-28 14:24:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (27.22%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.c
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.c
@@ -2,16 +2,11 @@ int findMin(int* a, int n) {
     int left=0, right=n-1;
     if(a[0] < a[right])
         return a[0];
-    while(left<right){
+    while(left<=right){
         int mid=(left+right)/2;
-        if(a[mid] > a[mid+1])
-            return a[mid+1];
-        else if(a[mid-1] > a[mid])
-            return a[mid];
-        else if(a[mid] > a[0])
+        if(a[mid] > a[n-1])
             left=mid+1;
-        else
-            right=mid-1;
+        else right=mid-1;
     }
-    return a[0];
+    return a[left];
 }
