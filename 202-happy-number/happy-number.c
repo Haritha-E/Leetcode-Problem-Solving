d504c9ef15ce83a95ffdc5f7f6abb8abcc6e5fbf
Haritha Eswaran <125653563+Haritha-E@users.noreply.github.com>
2024-12-31 09:18:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (42.16%) - LeetSync

--- 202-happy-number/happy-number.c
+++ 202-happy-number/happy-number.c
@@ -1,18 +1,15 @@
 bool isHappy(int n) {
-    int sum=0;
-    while(1){
-      sum=0; 
-      while(n!=0){
-        sum+=(n%10)*(n%10);
-        n=n/10;
-      }
-      if(sum>9)
-         n=sum;
-      else
-        break;
+    int sum,i=0;
+    while(i<10){
+        sum=0; 
+        while(n!=0){
+            sum+=(n%10)*(n%10);
+            n=n/10;
+        }
+        n=sum;
+        if(sum==1)
+            return true;
+        i++;
     }
-    if(sum==1 || sum==7)
-        return true;
-    else
-        return false;
+    return false;
 }
