7ff90459d9bf9d050ef085ed3794cb5800e691a7
Haritha E <harithaeswaran@gmail.com>
2025-01-22 11:30:33 +0530

    Time: 3 ms (31.22%) | Memory: 16.5 MB (5.35%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sol(int i, int j, int m, int n, vector<vector<int>>& g, vector<vector<int>>& dp){
+        if(i>=m || j>=n) return INT_MAX;
+        if(i==m-1 && j==n-1) return g[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j] = g[i][j] + min(sol(i+1,j,m,n,g,dp),sol(i,j+1,m,n,g,dp));
+    }
+    int minPathSum(vector<vector<int>>& g) {
+        int m=g.size();
+        int n=g[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return sol(0,0,m,n,g,dp);
+    }
+};
